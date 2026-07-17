09eb46e32aba4ec99198a385916f3f5d92b5dc56
hendi.dp <hendi.dp@pttmg.com>
2026-07-17 13:28:29 +0700

    added WhyUsSection

--- src/sections/MainSection.jsx
+++ src/sections/MainSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import AboutSection from './AboutSection'
 import CountsSection from './CountsSection'
+import WhyUsSection from './WhyUsSection'
 
 function MainSection() {
   return (
@@ -9,6 +10,7 @@ function MainSection() {
        <HeroSection></HeroSection>
        <AboutSection></AboutSection>
        <CountsSection></CountsSection>
+       <WhyUsSection></WhyUsSection>
       </div>
   )
 }
