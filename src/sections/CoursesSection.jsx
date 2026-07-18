7ef81010620c69fe0349bcb713898b083d30587c
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-07-18 11:18:51 +0700

    added courses data file

--- src/sections/CoursesSection.jsx
+++ src/sections/CoursesSection.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import course1 from '/src/assets/img/course/course-1.webp';
-import person7 from '/src/assets/img/person/person-m-7.webp';
-import course2 from '/src/assets/img/course/course-2.webp';
-import person14 from '/src/assets/img/person/person-f-14.webp';
-import course3 from '/src/assets/img/course/course-3.webp';
-import person12 from '/src/assets/img/person/person-m-12.webp';
+import { data } from '../datas/courses'
 
 function CoursesSection() {
   return (
@@ -17,84 +12,34 @@ function CoursesSection() {
             <div className="container">
 
                 <div className="row">
-
-                <div className="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                { data.map((item)=>(
+              
+                <div key={item.id} className="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                     <div className="course-item">
-                    <img src={course1} className="img-fluid" alt="..."/>
+                    <img src={item.thumbnail} className="img-fluid" alt="..."/>
                     <div className="course-content">
                         <div className="d-flex justify-content-between align-items-center mb-3">
-                        <p className="category">Web Development</p>
-                        <p className="price">$169</p>
+                        <p className="category">{item.category}</p>
+                        <p className="price">${item.price}</p>
                         </div>
 
-                        <h3><a href="course-details.html">Website Design</a></h3>
-                        <p className="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                        <h3><a href="course-details.html">{item.title}</a></h3>
+                        <p className="description">{item.desciption}</p>
                         <div className="trainer d-flex justify-content-between align-items-center">
                         <div className="trainer-profile d-flex align-items-center">
-                            <img src={person7} className="img-fluid" alt=""/>
-                            <a href="" className="trainer-link">Antonio</a>
+                            <img src={item.instructorImg} className="img-fluid" alt=""/>
+                            <a href="" className="trainer-link">{item.name}</a>
                         </div>
                         <div className="trainer-rank d-flex align-items-center">
-                            <i className="bi bi-person user-icon"></i>&nbsp;50
+                            <i className="bi bi-person user-icon"></i>&nbsp;{item.students}
                             &nbsp;&nbsp;
-                            <i className="bi bi-heart heart-icon"></i>&nbsp;65
+                            <i className="bi bi-heart heart-icon"></i>&nbsp;{item.likes}
                         </div>
                         </div>
                     </div>
                     </div>
                 </div>
-
-                <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-                    <div className="course-item">
-                    <img src={course2} className="img-fluid" alt="..."/>
-                    <div className="course-content">
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                        <p className="category">Marketing</p>
-                        <p className="price">$250</p>
-                        </div>
-
-                        <h3><a href="course-details.html">Search Engine Optimization</a></h3>
-                        <p className="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                        <div className="trainer d-flex justify-content-between align-items-center">
-                        <div className="trainer-profile d-flex align-items-center">
-                            <img src={person14} className="img-fluid" alt=""/>
-                            <a href="" className="trainer-link">Lana</a>
-                        </div>
-                        <div className="trainer-rank d-flex align-items-center">
-                            <i className="bi bi-person user-icon"></i>&nbsp;35
-                            &nbsp;&nbsp;
-                            <i className="bi bi-heart heart-icon"></i>&nbsp;42
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
-                    <div className="course-item">
-                    <img src={course3} className="img-fluid" alt="..."/>
-                    <div className="course-content">
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                        <p className="category">Content</p>
-                        <p className="price">$180</p>
-                        </div>
-
-                        <h3><a href="course-details.html">Copywriting</a></h3>
-                        <p className="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                        <div className="trainer d-flex justify-content-between align-items-center">
-                        <div className="trainer-profile d-flex align-items-center">
-                            <img src={person12} className="img-fluid" alt=""/>
-                            <a href="" className="trainer-link">Brandon</a>
-                        </div>
-                        <div className="trainer-rank d-flex align-items-center">
-                            <i className="bi bi-person user-icon"></i>&nbsp;20
-                            &nbsp;&nbsp;
-                            <i className="bi bi-heart heart-icon"></i>&nbsp;85
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                ))}
 
                 </div>
 
